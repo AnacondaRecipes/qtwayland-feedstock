bea45f504c723f728b7be314ebcada057e338491
Andrii-Osipov <aosipov@anaconda.com>
2026-02-11 02:10:49 -0800

    qtwayland 6.10.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,10 +5,13 @@ then
   CMAKE_ARGS="${CMAKE_ARGS} -DQT_HOST_PATH=${BUILD_PREFIX} -DQT_FORCE_BUILD_TOOLS=ON"
 fi
 
+export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include/qt6/QtWaylandGlobal/${PKG_VERSION}"
+
 cmake -LAH --log-level STATUS -G "Ninja" -B build ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
-  -DCMAKE_INSTALL_RPATH:STRING=${PREFIX}/lib \
+  -DCMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib" \
   -DCMAKE_UNITY_BUILD=ON \
   -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
    .
+
 cmake --build build --target install
